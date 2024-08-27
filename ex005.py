298593cb6d6c305b34a9b3f963dfeaf297f25b16
Bruna Grassi <101432594+BrunaGrassi@users.noreply.github.com>
2024-08-27 19:00:38 -0300

    exercícios práticos

---
+++ ex005.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número: '))
+
+print(f'O número informado foi: {n1}\nSeu antecessor é: {n1-1}\nSeu sucessor é: {n1+1}')
+print(f'O dobro é: {n1*2}\nA raiz quadrada é: {n1**(1/2)}')
+
